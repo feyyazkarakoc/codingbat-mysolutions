533dab08af5a66eb3db81c636193778e65402a67
Feyyaz Karakoç <fyyzkrkc@gmail.com>
2024-07-16 23:51:52 +0300

    commit

--- src/main/java/String2/XyzMiddle.java
+++ src/main/java/String2/XyzMiddle.java
@@ -4,17 +4,36 @@ public class XyzMiddle {
 
     public static void main(String[] args) {
 
-        System.out.println(xyzMiddle("xyzAxyzBBB"));
-        System.out.println(xyzMiddle("xyzxyzAxyzBxyzxyz"));
+        System.out.println(xyzMiddle("AAxyzBB"));
+        System.out.println(xyzMiddle("AxyzBB"));
         System.out.println(xyzMiddle("AxyzBBB"));
 
 
     }
 
 
+
     //solution
 
+
     public static boolean xyzMiddle(String str) {
+
+        for (int i = 0; i < str.length() - 2; i++) {
+            if (str.substring(i, i + 3).equals("xyz")) {
+                int leftLen = str.substring(0, i).length();
+                int rightLen = str.substring(i + 3).length();
+                if (Math.abs(leftLen - rightLen) <= 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+
+
+   /* public static boolean xyzMiddle(String str) {
 
         for (int i = 0; i < str.length()-2 ; i++) {
             if (str.substring(i,i+3).equals("xyz")){
@@ -25,5 +44,8 @@ public class XyzMiddle {
             }
         }
         return false;
-    }
+    }*/
+
+
+
 }
