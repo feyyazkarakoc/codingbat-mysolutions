5cbab5418392094610ef61f6d1c6029a0931b3d1
Feyyaz Karakoç <fyyzkrkc@gmail.com>
2024-07-17 00:43:15 +0300

    solution

--- src/main/java/String2/GetSandwich.java
+++ src/main/java/String2/GetSandwich.java
@@ -4,7 +4,25 @@ public class GetSandwich {
 
     public static void main(String[] args) {
 
+        System.out.println(getSandwich("breadjambread"));
+        System.out.println(getSandwich("xxbreadjambreadyy"));
+        System.out.println(getSandwich("xxbreadyy"));
 
 
     }
+
+
+    //solution
+
+    public static String getSandwich(String str) {
+        if (str.indexOf("bread")!=str.lastIndexOf("bread")){
+            for (int i = 0; i < str.length() ; i++) {
+                if (str.substring(i,i+5).equals("bread")){
+                    return str.substring(i+5,str.lastIndexOf("bread"));
+                }
+            }
+        }
+        return "";
+    }
+
 }
