7acd654d5f32da708f9b40b828952c191924b831
Feyyaz Karakoç <fyyzkrkc@gmail.com>
2024-07-18 01:05:58 +0300

    commit

--- src/main/java/String2/ZipZap.java
+++ src/main/java/String2/ZipZap.java
@@ -11,19 +11,58 @@ public class ZipZap {
 
     }
 
-    //solution
 
-    public static String zipZap(String str) {
+
+
+    //solution-1
+
+
+   public static String zipZap(String str) {
+        return str.replaceAll("z.p","zp");
+   }
+
+
+    //solution-2
+
+  /*  public static String zipZap(String str) {
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < str.length()-2 ; i++) {
-            sb.append(str.charAt(i));
-            if (str.charAt(i)=='z'&&str.charAt(i+2)=='p'){
-                i++;
+        int len = str.length();
+
+        for (int i = 0; i < len ; i++) {
+
+            if (i<len-2 && str.charAt(i)=='z' && str.charAt(i+2)=='p'){
+                sb.append("zp");
+                i+=2;
+            }else {
+                sb.append(str.charAt(i));
             }
-
         }
-        return sb.toString()+str.charAt(str.length()-1);
+        return sb.toString();
+    }*/
 
-    }
+
+
+
+
+
+
+  /*  public static String zipZap(String str) {
+        return str.replaceAll("z.p","zp");
+    }*/
+
+   /* public static String zipZap(String str) {
+        StringBuilder sb = new StringBuilder();
+        int len = str.length();
+        for (int i = 0; i < str.length() ; i++) {
+            if (i<len-2 && str.charAt(i)=='z' && str.charAt(i+2)=='p'){
+                sb.append("zp");
+                i+=2;
+            }else {
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
+    }*/
+
 }
