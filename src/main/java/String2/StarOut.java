f826b0741c9fb8ec1025764fd10e7159ec0b0a33
Feyyaz Karakoç <fyyzkrkc@gmail.com>
2024-07-18 01:52:22 +0300

    solution

---
+++ src/main/java/String2/StarOut.java
@@ -0,0 +1,62 @@
+package String2;
+
+public class StarOut {
+
+    public static void main(String[] args) {
+
+        System.out.println(starOut("ab*cd"));
+        System.out.println(starOut("ab**cd"));
+        System.out.println(starOut("sm*eilly"));
+
+    }
+
+
+    //solution
+
+    public static String starOut(String str) {
+        StringBuilder sb = new StringBuilder();
+        int len = str.length();
+        for (int i = 0; i < len ; i++) {
+            if (str.charAt(i)=='*' || (i>0 && str.charAt(i-1)=='*') || (i<len-1 && str.charAt(i+1)=='*')){
+                continue;
+            }
+            sb.append(str.charAt(i));
+        }
+        return sb.toString();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   /* public static String starOut(String str) {
+        StringBuilder sb = new StringBuilder();
+        int len = str.length();
+
+        for (int i = 0; i < len; i++) {
+
+            if (str.charAt(i) == '*' || (i > 0 && str.charAt(i - 1) == '*') || (i < len - 1 && str.charAt(i + 1) == '*')) {
+                continue;
+            }
+            sb.append(str.charAt(i));
+        }
+        return sb.toString();
+    }*/
+
+}
