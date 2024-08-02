78c34a89c58ab3bc48a8bdd573e57c5f3c9f5726
Feyyaz Karakoç <fyyzkrkc@gmail.com>
2024-08-03 00:35:35 +0300

    commit

--- src/main/java/String2/RepeatEnd.java
+++ src/main/java/String2/RepeatEnd.java
@@ -13,13 +13,22 @@ public class RepeatEnd {
     //solution
 
     public static String repeatEnd(String str, int n) {
-
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n ; i++) {
           sb.append(str.substring(str.length()-n));
         }
-
         return sb.toString();
-
     }
+
+
+
+   /* public static String repeatEnd(String str, int n) {
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n; i++){
+            sb.append(str.substring(str.length()-n));
+        }
+        return sb.toString();
+    }*/
+
 }
