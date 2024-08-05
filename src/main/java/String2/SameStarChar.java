38bd4b202c11c177823512300cce72c8fa21091f
Feyyaz Karakoç <fyyzkrkc@gmail.com>
2024-08-06 00:41:04 +0300

    solution

--- src/main/java/String2/SameStarChar.java
+++ src/main/java/String2/SameStarChar.java
@@ -14,6 +14,40 @@ public class SameStarChar {
 
     //solution
 
+    public static boolean sameStarChar(String str) {
+        boolean isSame = true;
+
+        for (int i = 1; i < str.length() - 1; i++) {
+            if (str.charAt(i) == '*') {
+
+                if (str.charAt(i - 1) != str.charAt(i + 1)) {
+                    isSame = false;
+                    break;
+
+                }
+            }
+
+        }
+        return isSame;
+    }
+
+
+
+      /* public static boolean sameStarChar(String str) {
+
+        for (int i = 1; i < str.length()-1; i++) {
+            if (str.charAt(i)=='*'){
+                if (str.charAt(i-1)!=str.charAt(i+1)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }*/
+
+
+
+
    /* public static boolean sameStarChar(String str) {
 
         boolean isSameChar = false;
@@ -33,17 +67,11 @@ public class SameStarChar {
     }*/
 
 
-    public static boolean sameStarChar(String str) {
 
-        for (int i = 1; i < str.length()-1; i++) {
-            if (str.charAt(i)=='*'){
-                if (str.charAt(i-1)!=str.charAt(i+1)){
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
+
+
+
+
 
 }
 
