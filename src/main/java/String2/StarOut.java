39fa1db80e3dd95d14ce13992a68140c568433a2
Feyyaz Karakoç <fyyzkrkc@gmail.com>
2024-08-06 23:56:43 +0300

    fix

--- src/main/java/String2/StarOut.java
+++ src/main/java/String2/StarOut.java
@@ -15,12 +15,13 @@ public class StarOut {
 
     public static String starOut(String str) {
         StringBuilder sb = new StringBuilder();
-        int len = str.length();
-        for (int i = 0; i < len ; i++) {
-            if (str.charAt(i)=='*' || (i>0 && str.charAt(i-1)=='*') || (i<len-1 && str.charAt(i+1)=='*')){
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '*' || (i > 0 && str.charAt(i - 1) == '*') || (i < str.length() - 1 && str.charAt(i + 1) == '*')) {
                 continue;
+            } else {
+                sb.append(str.charAt(i));
+
             }
-            sb.append(str.charAt(i));
         }
         return sb.toString();
     }
@@ -32,20 +33,22 @@ public class StarOut {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
    /* public static String starOut(String str) {
+        StringBuilder sb = new StringBuilder();
+        int len = str.length();
+        for (int i = 0; i < len ; i++) {
+            if (str.charAt(i)=='*' || (i>0 && str.charAt(i-1)=='*') || (i<len-1 && str.charAt(i+1)=='*')){
+                continue;
+            }
+            sb.append(str.charAt(i));
+        }
+        return sb.toString();
+    }*/
+
+
+
+
+    /* public static String starOut(String str) {
         StringBuilder sb = new StringBuilder();
         int len = str.length();
 
