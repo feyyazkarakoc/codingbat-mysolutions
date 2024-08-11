abe33df5987b8ff13776f88979c22f7540d27bb3
Feyyaz Karakoç <fyyzkrkc@gmail.com>
2024-08-11 12:10:01 +0300

    commit

---
+++ src/main/java/Array2/FizzArray.java
@@ -0,0 +1,28 @@
+package Array2;
+
+import java.util.Arrays;
+
+public class FizzArray {
+
+    public static void main(String[] args) {
+
+        System.out.println(Arrays.toString(fizzArray(4)));
+        System.out.println(Arrays.toString(fizzArray(1)));
+        System.out.println(Arrays.toString(fizzArray(10)));
+
+    }
+
+
+
+    //solution
+    public static int[] fizzArray(int n) {
+        int[] nums  = new int[n];
+        if(n>0){
+            for(int i = 0; i<n;i++){
+                nums[i]=i;
+            }
+            return nums;
+        }
+        return new int[0];
+    }
+}
