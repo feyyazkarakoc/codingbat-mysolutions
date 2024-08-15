647eacdb11695f1f27935255a0aad813e2dffb0a
Feyyaz Karakoç <fyyzkrkc@gmail.com>
2024-08-16 00:22:05 +0300

    fix

--- src/main/java/Array2/Has77.java
+++ src/main/java/Array2/Has77.java
@@ -16,42 +16,45 @@ public class Has77 {
     //solution
 
 
-   /* public static boolean has77(int[] nums) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] == 7) {
-                if (nums[i + 1] == 7 || (i < nums.length - 2 && nums[i + 2] == 7)) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }*/
-
-
-   /* public static boolean has77(int[] nums) {
-        for (int i = 0; i < nums.length - 1; i++) {
-
-            if (nums[i] == 7) {
-                if (nums[i + 1] == 7 || (i < nums.length - 2 && nums[i + 2] == 7)) {
-                    return true;
-                }
-            }
-
-        }
-
-        return false;
-    }*/
-
-
     public static boolean has77(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == 7) {
+                if (nums[i + 1] == 7 || (i < nums.length - 2 && nums[i + 2] == 7)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+  /*  public static boolean has77(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] == 7) {
+                if ((i+1<nums.length&&nums[i + 1] == 7) || (i +2 < nums.length && nums[i + 2] == 7)) {
+                    return true;
+                }
+            }
+
+        }
+
+        return false;
+    }*/
+
+
+
+
+
+   /* public static boolean has77(int[] nums) {
         for (int i = 0; i < nums.length ; i++) {
 
-            if (nums[i]==7&&((i+1<nums.length&&nums[i+1]==7))||(i+2<nums.length&&nums[i+2]==7)){
+            if (nums[i]==7&&((i+1<nums.length&&nums[i+1]==7)||(i+2<nums.length&&nums[i+2]==7))){
                 return true;
             }
 
         }
 
         return false;
-    }
+    }*/
 }
