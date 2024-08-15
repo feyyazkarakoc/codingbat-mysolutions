a3410231d5751e0b7094cdbc2359e020692258a2
Feyyaz Karakoç <fyyzkrkc@gmail.com>
2024-08-15 23:49:35 +0300

    solution

---
+++ src/main/java/Array2/Has77.java
@@ -0,0 +1,44 @@
+package Array2;
+
+public class Has77 {
+
+    public static void main(String[] args) {
+
+        int[] num1 = {1, 7, 7};
+        int[] num2 = {1, 7, 1, 7};
+        int[] num3 = {1, 7, 1, 1, 7};
+        System.out.println(has77(num1));
+        System.out.println(has77(num2));
+        System.out.println(has77(num3));
+    }
+
+
+    //solution
+
+
+   /* public static boolean has77(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == 7) {
+                if (nums[i + 1] == 7 || (i < nums.length - 2 && nums[i + 2] == 7)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }*/
+
+
+    public static boolean has77(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+
+            if (nums[i] == 7) {
+                if (nums[i + 1] == 7 || (i < nums.length - 2 && nums[i + 2] == 7)) {
+                    return true;
+                }
+            }
+
+        }
+
+        return false;
+    }
+}
