856b61d8ef523f203083dd29c7b0d1064e4f7452
Feyyaz Karakoç <fyyzkrkc@gmail.com>
2024-09-03 14:31:29 +0300

    solution

--- src/main/java/Logic2/BlackJack.java
+++ src/main/java/Logic2/BlackJack.java
@@ -1,2 +1,31 @@
-package Logic2;public class BlackJack {
+package Logic2;
+
+public class BlackJack {
+
+    public static void main(String[] args) {
+
+        System.out.println(blackjack(19, 21));
+        System.out.println(blackjack(21, 19));
+        System.out.println(blackjack(19, 22));
+
+    }
+
+    //solution
+    public static int blackjack(int a, int b) {
+        if (a <= 21 && b > 21) return a;
+        if (b <= 21 && a > 21) return b;
+        if (a > b && a < 22) return a;
+        if (b > a && b < 22) return b;
+        return 0;
+    }
+
+
+
+
+  /*  public static int blackjack(int a, int b) {
+      if(a>21&&b>21) return 0;
+      if(a>21) return b;
+      if (b>21) return a;
+      return Math.max(a,b);
+    }*/
 }
--- src/main/java/Logic2/CloseFar.java
+++ src/main/java/Logic2/CloseFar.java
@@ -1,2 +1,19 @@
-package Logic2;public class CloseFar {
+package Logic2;
+
+public class CloseFar {
+
+    public static void main(String[] args) {
+
+        System.out.println(closeFar(1, 2, 10));
+        System.out.println(closeFar(1, 2, 3));
+        System.out.println(closeFar(4, 1, 3));
+
+    }
+
+    public static boolean closeFar(int a, int b, int c) {
+        int x = Math.abs(a - b);
+        int y = Math.abs(a - c);
+        int z = Math.abs(b - c);
+        return (x <= 1 && z >= 2 && y >= 2) || (y <= 1 && z >= 2 && x >= 2) ;
+    }
 }
