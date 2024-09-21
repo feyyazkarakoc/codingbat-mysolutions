2a300027188a241654fd67d2f0cc11db8ad05039
Feyyaz Karakoç <fyyzkrkc@gmail.com>
2024-09-21 18:58:09 +0300

    solution

--- src/main/java/Recursion2/GroupSum.java
+++ src/main/java/Recursion2/GroupSum.java
@@ -4,12 +4,12 @@ public class GroupSum {
 
     public static void main(String[] args) {
 
-        int[] arr = {2, 4, 8};
+        int[] nums = {2, 4, 8};
 
 
-        System.out.println(groupSum(0, arr, 10));
-        System.out.println(groupSum(0, arr, 14));
-        System.out.println(groupSum(0, arr, 19));
+        System.out.println(groupSum(0, nums, 10));
+        System.out.println(groupSum(0, nums, 14));
+        System.out.println(groupSum(0, nums, 19));
 
 
     }
@@ -17,8 +17,42 @@ public class GroupSum {
 
     //solution
     public static boolean groupSum(int start, int[] nums, int target) {
-        if (nums[start]>=nums.length) return true;
+
+        if (target == 0) return true;
+
+        if (start >= nums.length) return false;
+
+        if (groupSum(start + 1, nums, target - nums[start])) {
+            return true;
+        }
+
+        if (groupSum(start + 1, nums, target)) {
+            return true;
+        }
+
+        return false;
 
     }
+
+
+
+
+
+   /* public static boolean groupSum(int start, int[] nums, int target) {
+
+        if (target == 0) return true;
+
+        if (start >= nums.length) return false;
+
+        if (groupSum(start + 1, nums, target - nums[start])) {
+            return true;
+        }
+
+        if (groupSum(start + 1, nums, target)) {
+            return true;
+        }
+
+        return false;
+    }*/
 
 }
