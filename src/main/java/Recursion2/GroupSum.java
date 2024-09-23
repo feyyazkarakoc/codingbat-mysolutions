85dc75544a0914cf83ba14caf70780d0e3769813
Feyyaz Karakoç <fyyzkrkc@gmail.com>
2024-09-23 13:46:02 +0300

    solution

--- src/main/java/Recursion2/GroupSum.java
+++ src/main/java/Recursion2/GroupSum.java
@@ -16,7 +16,7 @@ public class GroupSum {
 
 
     //solution
-    public static boolean groupSum(int start, int[] nums, int target) {
+   /* public static boolean groupSum(int start, int[] nums, int target) {
 
         if (target == 0) return true;
 
@@ -31,6 +31,16 @@ public class GroupSum {
         }
 
         return false;
+
+    }*/
+
+
+    public static boolean groupSum(int start, int[] nums, int target) {
+
+        if (start >= nums.length) return target == 0;
+
+        return groupSum(start + 1, nums, target - nums[start]) ||
+                groupSum(start + 1, nums, target);
 
     }
 
---
+++ src/main/java/Recursion2/GroupSumClump.java
@@ -0,0 +1,67 @@
+package Recursion2;
+
+public class GroupSumClump {
+
+    public static void main(String[] args) {
+
+        int[] nums1 = {2, 4, 8};
+        int[] nums2 = {1, 2, 4, 8, 1};
+        int[] nums3 = {2, 4, 4, 8};
+
+
+        System.out.println(groupSumClump(0, nums1, 10));
+        System.out.println(groupSumClump(0, nums2, 14));
+        System.out.println(groupSumClump(0, nums3, 14));
+
+
+    }
+
+
+    //solution
+    public static boolean groupSumClump(int start, int[] nums, int target) {
+
+        if (start >= nums.length && target == 0) return true;
+
+        if (start >= nums.length) return false;
+
+        int sum = nums[start];
+
+        int count = 1;
+
+        while (start + count < nums.length && nums[start] == nums[start + count]) {
+            sum += nums[start + count];
+            count++;
+        }
+
+        if (groupSumClump(start + count, nums, target - sum)) {
+            return true;
+        }
+
+        if (groupSumClump(start + count, nums, target)) {
+            return true;
+        }
+
+        return false;
+    }
+
+
+
+
+  /*  public static boolean groupSumClump(int start, int[] nums, int target) {
+
+        if (start >= nums.length) return target == 0;
+
+        int sum = nums[start];
+
+        int count = 1;
+
+        while (start + count < nums.length && nums[start] == nums[start + count]) {
+            sum += nums[start + count];
+            count++;
+        }
+
+        return groupSumClump(start + count, nums, target - sum) ||
+                groupSumClump(start + count, nums, target);
+
+    }*/
+}
