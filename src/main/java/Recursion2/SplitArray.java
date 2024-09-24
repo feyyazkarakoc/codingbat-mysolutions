576ccdd8bbef687c813dd43f61b3ad8c73c5389f
Feyyaz Karakoç <fyyzkrkc@gmail.com>
2024-09-24 12:59:28 +0300

    solution

---
+++ src/main/java/Recursion2/SplitArray.java
@@ -0,0 +1,40 @@
+package Recursion2;
+
+public class SplitArray {
+
+    public static void main(String[] args) {
+
+        int[] nums1 = {2, 2};
+        int[] nums2 = {2, 3};
+        int[] nums3 = {5, 2, 3};
+
+
+        System.out.println(splitArray(nums1));
+        System.out.println(splitArray(nums2));
+        System.out.println(splitArray(nums3));
+
+
+    }
+
+
+    //solution
+    public static boolean splitArray(int[] nums) {
+
+        return splitArrayHelper(nums, 0, 0, 0);
+    }
+
+    public static boolean splitArrayHelper(int[] nums, int index, int group1Sum, int group2Sum) {
+
+        if (index == nums.length) return group1Sum == group2Sum;
+
+        if (splitArrayHelper(nums, index + 1, group1Sum + nums[index], group2Sum)) {
+            return true;
+        }
+
+        if (splitArrayHelper(nums, index + 1, group1Sum, group2Sum + nums[index])) {
+            return true;
+        }
+
+        return false;
+    }
+}
