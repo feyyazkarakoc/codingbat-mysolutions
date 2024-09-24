1fd4396c367da18d1bf4d7de6a15358cae29a072
Feyyaz Karakoç <fyyzkrkc@gmail.com>
2024-09-24 21:55:35 +0300

    solutions

---
+++ src/main/java/Recursion2/Split53.java
@@ -0,0 +1,84 @@
+package Recursion2;
+
+public class Split53 {
+
+    public static void main(String[] args) {
+
+        int[] nums1 = {1, 1};
+        int[] nums2 = {1, 1, 1};
+        int[] nums3 = {2, 4, 2};
+
+
+        System.out.println(split53(nums1));
+        System.out.println(split53(nums2));
+        System.out.println(split53(nums3));
+
+
+    }
+
+
+    //solution
+
+    public static boolean split53(int[] nums) {
+
+        return split53Helper(nums, 0, 0, 0);
+    }
+
+
+
+    public static boolean split53Helper(int[] nums, int index, int group1Sum, int group2Sum) {
+
+        if (index == nums.length) return group1Sum == group2Sum;
+
+        if (nums[index] % 5 == 0) {
+            return split53Helper(nums, index + 1, group1Sum + nums[index], group2Sum);
+        }
+
+        if (nums[index] % 3 == 0) {
+            return split53Helper(nums, index + 1, group1Sum, group2Sum + nums[index]);
+        }
+
+        if (split53Helper(nums, index + 1, group1Sum + nums[index], group2Sum)) {
+            return true;
+        }
+
+        if (split53Helper(nums, index + 1, group1Sum, group2Sum + nums[index])) {
+            return true;
+        }
+
+        return false;
+
+    }
+
+
+
+
+
+
+
+
+   /* public static boolean split53(int[] nums) {
+
+        return split53Helper(nums, 0, 0, 0);
+    }
+
+
+
+    public static boolean split53Helper(int[] nums, int index, int group1Sum, int group2Sum) {
+
+        if (index == nums.length) return group1Sum == group2Sum;
+
+        int current = nums[index];
+
+        if (current % 5 == 0) {
+            return split53Helper(nums, index + 1, group1Sum + current, group2Sum);
+        }
+
+        if (current % 3 == 0) {
+            return split53Helper(nums, index + 1, group1Sum, group2Sum + current);
+        }
+
+        return split53Helper(nums, index + 1, group1Sum + current, group2Sum) ||
+                split53Helper(nums, index + 1, group1Sum, group2Sum + current);
+    }*/
+}
---
+++ src/main/java/Recursion2/SplitOdd10.java
@@ -0,0 +1,41 @@
+package Recursion2;
+
+public class SplitOdd10 {
+
+    public static void main(String[] args) {
+
+        int[] nums1 = {5, 5, 5};
+        int[] nums2 = {5, 5, 6};
+        int[] nums3 = {5, 5, 6, 1};
+
+
+        System.out.println(splitOdd10(nums1));
+        System.out.println(splitOdd10(nums2));
+        System.out.println(splitOdd10(nums3));
+
+
+    }
+
+
+    //solution
+    public static boolean splitOdd10(int[] nums) {
+
+        return splitOdd10Helper(nums, 0, 0, 0);
+    }
+
+    public static boolean splitOdd10Helper(int[] nums, int index, int group1Sum, int group2Sum) {
+
+        if (index == nums.length) return group1Sum % 10 == 0 && group2Sum % 2 == 1;
+
+        if (splitOdd10Helper(nums, index + 1, group1Sum + nums[index], group2Sum)) {
+            return true;
+        }
+
+        if (splitOdd10Helper(nums, index + 1, group1Sum, group2Sum + nums[index])) {
+            return true;
+        }
+
+        return false;
+
+    }
+}
