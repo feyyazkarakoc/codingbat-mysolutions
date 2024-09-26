900d247a0c9eca77fcf0e80dcdba56d8bf5fd3e9
Feyyaz Karakoç <fyyzkrkc@gmail.com>
2024-09-27 00:51:02 +0300

    solution

--- src/main/java/Recursion2/GroupSum6.java
+++ src/main/java/Recursion2/GroupSum6.java
@@ -37,4 +37,30 @@ public class GroupSum6 {
         return false;
 
     }
+
+
+
+
+
+
+   /* public static boolean groupSum6(int start, int[] nums, int target) {
+
+        if (start >= nums.length && target == 0) return true;
+
+        if (start >= nums.length) return false;
+
+        if (nums[start] == 6) {
+            return groupSum6(start + 1, nums, target - nums[start]);
+        }
+
+        if (groupSum6(start + 1, nums, target - nums[start])) {
+            return true;
+        }
+
+        if (groupSum6(start + 1, nums, target)) {
+            return true;
+        }
+
+        return false;
+    }*/
 }
