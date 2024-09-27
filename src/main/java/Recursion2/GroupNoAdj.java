5a0a39b28b733c5c4d96455140dd8afdd5ec899a
Feyyaz Karakoç <fyyzkrkc@gmail.com>
2024-09-27 11:23:27 +0300

    solution

--- src/main/java/Recursion2/GroupNoAdj.java
+++ src/main/java/Recursion2/GroupNoAdj.java
@@ -33,4 +33,22 @@ public class GroupNoAdj {
         return false;
 
     }
+
+
+   /* public static boolean groupNoAdj(int start, int[] nums, int target) {
+
+        if (target == 0) return true;
+
+        if (start >= nums.length) return false;
+
+        if (groupNoAdj(start + 2, nums, target - nums[start])) {
+            return true;
+        }
+
+        if (groupNoAdj(start + 1, nums, target)) {
+            return true;
+        }
+
+        return false;
+    }*/
 }
