3b38911156a0b84fa0efb1ec26e08fbafc04143e
Feyyaz Karakoç <fyyzkrkc@gmail.com>
2024-10-05 01:07:37 +0300

    solution

--- src/main/java/String3/GHappy.java
+++ src/main/java/String3/GHappy.java
@@ -15,16 +15,37 @@ public class GHappy {
 
     public static boolean  gHappy(String str) {
 
-        boolean isHappy = false;
         for (int i = 0; i < str.length(); i++) {
 
             if (str.charAt(i) == 'g') {
-                isHappy = i + 1 < str.length() && str.charAt(i + 1) == 'g';
+                if ((i + 1 < str.length() && str.charAt(i + 1) == 'g') ||
+                        (i - 1 >= 0 && str.charAt(i - 1) == 'g')) {
+                    continue;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+
+
+
+
+
+   /* public static boolean gHappy(String str) {
+
+        for (int i = 0; i < str.length(); i++) {
+
+            if (str.charAt(i) == 'g') {
+                if (!((i - 1 >= 0 && str.charAt(i - 1) == 'g') || (i + 1 < str.length() && str.charAt(i + 1) == 'g'))) {
+                    return false;
+                }
             }
 
         }
-
-        return isHappy;
-
-    }
+        return true;
+    }*/
 }
