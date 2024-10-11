7b10257f24afc9410c6078f74732d476680dc3ef
Feyyaz Karakoç <fyyzkrkc@gmail.com>
2024-10-11 16:53:56 +0300

    solution

---
+++ src/main/java/String3/SameEnds.java
@@ -0,0 +1,21 @@
+package String3;
+
+public class SameEnds {
+
+    public static void main(String[] args) {
+
+        System.out.println( sameEnds("abXYab"));
+        System.out.println( sameEnds("xx"));
+        System.out.println( sameEnds("xxx"));
+
+    }
+
+
+
+    //solutions
+
+    public static int  sameEnds(String str) {
+
+
+    }
+}
