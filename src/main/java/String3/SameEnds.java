96932d3fec22ccd05927b9d03e00db1126d75063
Feyyaz Karakoç <fyyzkrkc@gmail.com>
2024-10-19 23:52:42 +0300

    solution

--- src/main/java/String3/SameEnds.java
+++ src/main/java/String3/SameEnds.java
@@ -14,7 +14,26 @@ public class SameEnds {
 
     //solutions
 
-    public static int  sameEnds(String str) {
+    public static String sameEnds(String str) {
+
+        StringBuilder sb = new StringBuilder();
+        if (!str.isEmpty()){
+
+            for (int i = 0; i < str.length()-1 ; i++) {
+
+                if (str.charAt(i)==str.charAt(str.length()-1-i)){
+                    sb.append(str.charAt(i));
+
+                    if (str.charAt(i+1)!=str.charAt(str.length()-2-i)){
+                        break;
+                    }
+                }
+
+            }
+
+        }
+
+        return sb.toString();
 
 
     }
