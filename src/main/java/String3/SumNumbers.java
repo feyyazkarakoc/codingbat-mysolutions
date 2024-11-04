8b0de288470bddd0d5e62016193097a6b9fa2467
Feyyaz Karakoç <fyyzkrkc@gmail.com>
2024-11-04 14:31:33 +0300

    solution

--- src/main/java/String3/SumNumbers.java
+++ src/main/java/String3/SumNumbers.java
@@ -1,2 +1,103 @@
-package String3;public class SumNumbers {
+package String3;
+
+public class SumNumbers {
+
+    public static void main(String[] args) {
+
+        System.out.println(sumDigits("abc123xyz"));
+        System.out.println(sumDigits("aa11b33"));
+        System.out.println(sumDigits("7 11"));
+
+    }
+
+
+    //solutions
+
+
+    public static int sumDigits(String str) {
+
+        StringBuilder sb = new StringBuilder();
+        int sum = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+
+            if (Character.isDigit(ch)) {
+
+                sb.append(ch);
+
+            } else if (sb.length() > 0) {
+
+                sum += Integer.parseInt(sb.toString());
+                sb = new StringBuilder();
+            }
+
+        }
+
+        if (sb.length() > 0) {
+
+            sum += Integer.parseInt(sb.toString());
+        }
+
+        return sum;
+    }
+
+
+
+
+
+  /*  public static int sumDigits(String str) {
+
+
+        StringBuilder sb = new StringBuilder();
+        int sum = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isDigit(str.charAt(i))) {
+
+                while (i < str.length() && Character.isDigit(str.charAt(i))) {
+
+                    sb.append(str.charAt(i));
+                    i++;
+                }
+
+                sum += Integer.parseInt(sb.toString());
+                sb = new StringBuilder();
+                i--;
+
+            }
+        }
+
+        return sum;
+    }*/
+
+
+
+
+   /* public static int sumDigits(String str) {
+
+        StringBuilder sb = new StringBuilder();
+        int sum = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            if (Character.isDigit(str.charAt(i))) {
+
+                while (i < str.length() && Character.isDigit(str.charAt(i))) {
+
+                    sb.append(str.charAt(i));
+                    i++;
+                }
+
+                sum += Integer.parseInt(sb.toString());
+                sb = new StringBuilder();
+                i--;
+            }
+
+        }
+
+        return sum;
+
+    }*/
 }
