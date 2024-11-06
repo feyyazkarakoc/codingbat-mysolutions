df0b24e001f04377524081c17a58373eb016d4fa
Feyyaz Karakoç <fyyzkrkc@gmail.com>
2024-11-06 20:05:47 +0300

    solution

---
+++ src/main/java/Array1/MakePi.java
@@ -0,0 +1,21 @@
+package Array1;
+
+import java.util.Arrays;
+
+public class MakePi {
+
+    public static void main(String[] args) {
+
+        System.out.println(Arrays.toString(makePi()));
+
+
+    }
+
+
+    //solution
+    public static int[] makePi() {
+
+        return new int[]{3, 1, 4};
+
+    }
+}
