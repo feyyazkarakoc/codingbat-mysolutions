8e3582fb1e679b0a20df17b5207025f013ccc9b7
Feyyaz Karakoç <fyyzkrkc@gmail.com>
2024-11-14 23:36:36 +0300

    solution

---
+++ src/main/java/Array1/Double23.java
@@ -0,0 +1,22 @@
+package Array1;
+
+import java.util.Arrays;
+
+public class Double23 {
+
+    public static void main(String[] args) {
+
+        System.out.println(double23(new int[]{2, 2}));
+        System.out.println(double23(new int[]{3, 3}));
+        System.out.println(double23(new int[]{2, 3}));
+
+    }
+
+
+    //solution
+    public static boolean double23(int[] nums) {
+
+        return nums.length == 2 && ((nums[0] == 2 && nums[1] == 2) || (nums[0] == 3 && nums[1] == 3));
+
+    }
+}
