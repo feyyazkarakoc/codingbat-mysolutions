4477d7ede7a625d0bb42be44ab560f9d67df5294
Feyyaz Karakoç <fyyzkrkc@gmail.com>
2024-12-13 23:49:58 +0300

    solutions

--- src/main/java/Array1/Front11.java
+++ src/main/java/Array1/Front11.java
@@ -6,21 +6,43 @@ public class Front11 {
 
     public static void main(String[] args) {
 
-        System.out.println(Arrays.toString(front11(new int[]{1, 3, 4, 5}, new int[]{1, 3, 4, 5})));
-        System.out.println(Arrays.toString(front11(new int[]{2, 1, 3, 4, 5}, new int[]{1, 3, 4, 5})));
-        System.out.println(Arrays.toString(front11(new int[]{1, 1, 1}, new int[]{1, 3, 4, 5})));
+        System.out.println(Arrays.toString(front11(new int[]{1, 2, 3}, new int[]{7, 9, 8})));
+        System.out.println(Arrays.toString(front11(new int[]{1}, new int[]{2})));
+        System.out.println(Arrays.toString(front11(new int[]{1, 7}, new int[]{})));
 
     }
 
 
     //solution
+   /* public static int[] front11(int[] a, int[] b) {
+
+        return a.length >= 1 && b.length >= 1
+                ? new int[]{a[0], b[0]}
+                : (a.length >= 1
+                ? new int[]{a[0]}
+                : (b.length >= 1
+                ? new int[]{b[0]}
+                : new int[]{}));
+
+    }*/
+
+
     public static int[] front11(int[] a, int[] b) {
 
-        return a.length >= 2
-                ? new int[]{a[0], a[1]}
-                : (a.length == 1
-                ? new int[]{a[0], b[0]}
-                : new int[]{b[0], b[1]});
+        if (a.length == 0 && b.length == 0) {
+            return new int[]{};
+        }
 
+        if (a.length == 0) {
+            return new int[]{b[0]};
+        }
+
+        if (b.length == 0) {
+            return new int[]{a[0]};
+        }
+
+        return new int[]{a[0], b[0]};
     }
+
+
 }
