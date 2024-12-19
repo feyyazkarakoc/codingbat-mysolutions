0ac07460ac6d4e4a2185e0481ec1eb259298f49b
Feyyaz Karakoç <fyyzkrkc@gmail.com>
2024-12-19 18:29:27 +0300

    solution

---
+++ src/main/java/Array3/Fix34.java
@@ -0,0 +1,142 @@
+package Array3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Fix34 {
+
+    public static void main(String[] args) {
+
+        System.out.println(Arrays.toString(fix34(new int[]{1, 3, 1, 4})));
+        System.out.println(Arrays.toString(fix34(new int[]{1, 3, 1, 4, 4, 3, 1})));
+        System.out.println(Arrays.toString(fix34(new int[]{3, 2, 2, 4})));
+
+    }
+
+
+    // solution 1 :
+    public static int[] fix34(int[] nums) {
+
+        int len = nums.length;
+        int index4 = 0;
+
+        for (int i = 0; i < len; i++) {
+
+            if (nums[i] == 3) {
+
+                while (nums[index4] != 4) {
+                    index4++;
+                }
+
+                int temp = nums[i + 1];
+                nums[i + 1] = nums[index4];
+                nums[index4] = temp;
+
+            }
+        }
+
+        return nums;
+    }
+
+
+
+
+
+    // solution 2:
+   /* public static int[] fix34(int[] nums) {
+
+        int[] pos4 = new int[nums.length];
+        int pos4index = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] == 4) {
+                pos4[pos4index++] = i;
+
+            }
+
+        }
+
+        int index4 = 0;
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] == 3) {
+                int temp = nums[i + 1];
+                nums[i + 1] = nums[pos4[index4]];
+                nums[pos4[index4++]] = temp;
+                i++;
+            }
+
+        }
+        return nums;
+    }*/
+
+
+
+    // solution 3 (non-best practice solution):
+   /* public static int[] fix34(int[] nums) {
+
+        int len = nums.length;
+
+        int[] newArr = new int[len];
+
+        for (int i = 0; i < len; i++) {
+            if (nums[i] == 3) {
+                newArr[i] = nums[i];
+                newArr[i + 1] = 4;
+                i++;
+            }
+        }
+
+        for (int num : nums) {
+
+            if (num != 3 && num != 4) {
+
+                for (int j = 0; j < len; j++) {
+
+                    if (newArr[j] != 3 && newArr[j] != 4) {
+                        newArr[j] = num;
+                    }
+                }
+
+            }
+
+        }
+
+        return newArr;
+    }*/
+
+
+
+
+
+
+
+
+    /*public static int[] fix34(int[] nums) {
+
+        int len = nums.length;
+        int index4 = 0;
+
+        for (int i = 0; i < len; i++) {
+
+            if (nums[i] == 3) {
+
+                while (nums[index4] != 4) {
+                    index4++;
+                }
+
+
+                int tempt = nums[i + 1];
+                nums[i + 1] = nums[index4];
+                nums[index4] = tempt;
+            }
+
+        }
+
+        return nums;
+    }*/
+
+
+}
--- src/main/java/Array3/MaxSpan.java
+++ src/main/java/Array3/MaxSpan.java
@@ -15,28 +15,28 @@ public class MaxSpan {
 
 
     // solution 1:
-     public static int maxSpan(int[] nums){
+    public static int maxSpan(int[] nums) {
 
         int maxSpan = 0;
 
-         for (int i = 0; i < nums.length ; i++) {
+        for (int i = 0; i < nums.length; i++) {
 
-             for (int j = nums.length-1; j >=i ; j--) {
+            for (int j = nums.length - 1; j >= i; j--) {
 
-                 if (nums[i]==nums[j]){
+                if (nums[i] == nums[j]) {
 
-                     maxSpan = Math.max(maxSpan,j-i+1);
-                     break;
+                    maxSpan = Math.max(maxSpan, j - i + 1);
+                    break;
 
-                 }
+                }
 
 
-             }
+            }
 
-         }
+        }
 
-         return maxSpan;
-     }
+        return maxSpan;
+    }
 
 
     // solution 2 :
