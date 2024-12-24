4fee610ff0546875266b434298cf31d91f7d0e76
Feyyaz Karakoç <fyyzkrkc@gmail.com>
2024-12-24 23:05:45 +0300

    solution

---
+++ src/main/java/Array3/SquareUp.java
@@ -0,0 +1,30 @@
+package Array3;
+
+import java.util.Arrays;
+
+public class SquareUp {
+
+    public static void main(String[] args) {
+
+        System.out.println(Arrays.toString(squareUp(3)));
+        System.out.println(Arrays.toString(squareUp(2)));
+        System.out.println(Arrays.toString(squareUp(4)));
+
+    }
+
+
+    // solution 1:
+    public static int[] squareUp(int n) {
+
+        int[] result = new int[n * n];
+
+        for (int row = 1; row <= n; row++) {
+            int start = row * n - 1;
+            for (int num = 1; num <= row; num++) {
+                result[start--] = num;
+            }
+        }
+
+        return result;
+    }
+}
