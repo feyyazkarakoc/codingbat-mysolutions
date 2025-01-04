36dfb00bb087070ec1fc7f152dca692f594d2bff
Feyyaz Karakoç <fyyzkrkc@gmail.com>
2025-01-04 12:11:00 +0300

    solutions

---
+++ src/main/java/Warmup1/SleepIn.java
@@ -0,0 +1,19 @@
+package Warmup1;
+
+public class SleepIn {
+
+
+    public static void main(String[] args) {
+
+        System.out.println(sleepIn(false, false));
+        System.out.println(sleepIn(true, false));
+        System.out.println(sleepIn(false, true));
+    }
+
+
+    public static boolean sleepIn(boolean weekday, boolean vacation) {
+        return !weekday || vacation;
+    }
+
+
+}
