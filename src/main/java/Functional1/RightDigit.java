76ab5843958cd18b5b40b5ed3760f04a28ef293c
Feyyaz Karakoç <fyyzkrkc@gmail.com>
2025-01-24 23:59:33 +0300

    solution

--- src/main/java/Functional1/RightDigit.java
+++ src/main/java/Functional1/RightDigit.java
@@ -1,18 +1,26 @@
 package Functional1;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RightDigit {
 
 
     public static void main(String[] args) {
 
+        System.out.println(rightDigit(new ArrayList<>(List.of(1, 22, 93))));
+        System.out.println(rightDigit(new ArrayList<>(List.of(16, 8, 886, 8, 1))));
+        System.out.println(rightDigit(new ArrayList<>(List.of(10, 0))));
 
     }
 
 
     public static List<Integer> rightDigit(List<Integer> nums) {
 
+        return nums.stream()
+                .map(t -> t % 10)
+                .collect(Collectors.toList());
     }
 
 }
